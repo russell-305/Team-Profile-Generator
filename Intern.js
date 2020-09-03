8462bbabd43ed080483fd1b5cd608c48d93c3d95
russell-305 <64663543+russell-305@users.noreply.github.com>
2020-09-03 00:01:16 -0400

    Add files via upload

---
+++ Intern.js
@@ -0,0 +1,9 @@
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+class Intern {
+
+    constructor(name,duty) {
+      this.name = name;
+      this.duty = duty; 
+    }
+}
+module.exports = Intern;
