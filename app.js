d5278b94b4e45adcc4b4f42e0f87f899dce8e189
russell-305 <64663543+russell-305@users.noreply.github.com>
2020-09-09 17:38:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,124 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+const renderEngineer = require("./lib/htmlRenderer");
 
+const staff = [];
+
+const getRole = () => {
+  inquirer
+    .prompt({
+      name: "role",
+      type: "list",
+      message: "Please select your Employee title below:",
+      choices: [
+        { name: "Intern", value: "intern" },
+        { name: "Engineer", value: "engineer" },
+        { name: "Manager", value: "manager" },
+      ],
+    })
+    .then((answer) => {
+      console.log(answer);
+      if (answer.role === "intern") {
+        return getIntern();
+      } else if (answer.role === "engineer") {
+        return getEngineer();
+      } else if (answer.role === "manager") {
+        return getManager();
+      }
+    });
+};
+
+const getIntern = () => {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Please enter your first and last name:",
+      },
+      { name: "id", type: "input", message: "Please enter your ID number:" },
+      { name: "email", type: "input", message: "Enter your email:" },
+      {
+        name: "college",
+        type: "input",
+        message: "Please enter your university:",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      staff.push(new Intern(data.name, data.id, data.email, data.college));
+      closeOut();
+    });
+};
+
+const getEngineer = () => {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Please enter your first and last name:",
+      },
+      { name: "id", type: "input", message: "Please enter your ID number:" },
+      { name: "email", type: "input", message: "Enter your email:" },
+      {
+        name: "github",
+        type: "input",
+        message: "Please enter your GitHub username:",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      staff.push(new Engineer(data.name, data.id, data.email, data.github));
+
+      closeOut();
+    });
+};
+
+const getManager = () => {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Please enter your first and last name:",
+      },
+      { name: "id", type: "input", message: "Please enter your ID number:" },
+      { name: "email", type: "input", message: "Enter your email:" },
+      {
+        name: "officeNum",
+        type: "input",
+        message: "Please enter your office number:",
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      staff.push(new Manager(data.name, data.id, data.email, data.officeNum));
+      closeOut(data);
+    });
+};
+
+const closeOut = () => {
+  inquirer
+    .prompt([
+      {
+        name: "next",
+        type: "confirm",
+        message: "Would you like to add another Employee?",
+      },
+    ])
+    .then((ans) => {
+      if (ans.next === true) {
+        getRole();
+      } else {
+        render(staff);
+        console.log(staff);
+      }
+    });
+};
+getRole();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
