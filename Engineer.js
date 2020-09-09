b5fbc4f1a3eb6299b672c208cea70865d17db9ee
russell-305 <64663543+russell-305@users.noreply.github.com>
2020-09-09 17:28:33 -0400

    Update Engineer.js

--- Engineer.js
+++ Engineer.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer {
+    const Employee = require("./Employee");
+
+ class Engineer extends Employee 
+    constructor(name,id,email,github){
+      super(name,id,email)
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.github = github;
+
+
+       
+    };
+    getGithub(){
+      console.log(this.name);
+      console.log(this.email);
+      console.log(this.id);
+      return this.github;
+    };
+    
+    getRole(){
+      return "Engineer";
+
+    };
+}
+module.exports = Engineer;
