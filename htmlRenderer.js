2d8297b00ef171e30716ee7a13b61fab7311131d
russell-305 <64663543+russell-305@users.noreply.github.com>
2020-09-09 17:31:32 -0400

    Update htmlRenderer.js

--- htmlRenderer.js
+++ htmlRenderer.js
@@ -3,58 +3,94 @@ const fs = require("fs");
 
 const templatesDir = path.resolve(__dirname, "../templates");
 
-const render = employees => {
+const render = (employees) => {
   const html = [];
 
-  html.push(employees
-    .filter(employee => employee.getRole() === "Manager")
-    .map(manager => renderManager(manager))
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => renderManager(manager))
   );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Engineer")
-    .map(engineer => renderEngineer(engineer))
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => renderEngineer(engineer))
   );
-  html.push(employees
-    .filter(employee => employee.getRole() === "Intern")
-    .map(intern => renderIntern(intern))
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => renderIntern(intern))
   );
 
   return renderMain(html.join(""));
-
 };
 
-const renderManager = manager => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+const renderManager = (manager) => {
+  let template = fs.readFileSync(
+    path.resolve(templatesDir, "manager.html"),
+    "utf8"
+  );
   template = replacePlaceholders(template, "name", manager.getName());
   template = replacePlaceholders(template, "role", manager.getRole());
   template = replacePlaceholders(template, "email", manager.getEmail());
   template = replacePlaceholders(template, "id", manager.getId());
-  template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
-  return template;
+  template = replacePlaceholders(
+    template,
+    "officeNumber",
+    manager.getOfficeNumber()
+  );
+  return fs.writeFile(`${manager.name}.html`, template, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+  });
 };
 
-const renderEngineer = engineer => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+const renderEngineer = (engineer) => {
+  let template = fs.readFileSync(
+    path.resolve(templatesDir, "engineer.html"),
+    "utf8"
+  );
   template = replacePlaceholders(template, "name", engineer.getName());
   template = replacePlaceholders(template, "role", engineer.getRole());
   template = replacePlaceholders(template, "email", engineer.getEmail());
   template = replacePlaceholders(template, "id", engineer.getId());
   template = replacePlaceholders(template, "github", engineer.getGithub());
-  return template;
+  return fs.writeFile(`${engineer.name}.html`, template, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+  });
 };
 
-const renderIntern = intern => {
-  let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+const renderIntern = (intern) => {
+  let template = fs.readFileSync(
+    path.resolve(templatesDir, "intern.html"),
+    "utf8"
+  );
   template = replacePlaceholders(template, "name", intern.getName());
   template = replacePlaceholders(template, "role", intern.getRole());
   template = replacePlaceholders(template, "email", intern.getEmail());
   template = replacePlaceholders(template, "id", intern.getId());
   template = replacePlaceholders(template, "school", intern.getSchool());
-  return template;
+  return fs.writeFile(`${intern.name}.html`, template, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+  });
 };
 
-const renderMain = html => {
-  const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
+const renderMain = (html) => {
+  const template = fs.readFileSync(
+    path.resolve(templatesDir, "main.html"),
+    "utf8"
+  );
   return replacePlaceholders(template, "team", html);
 };
 
