1c1687dba1f1b6f8543d8f5f6b624aaf4117bdc2
russell-305 <64663543+russell-305@users.noreply.github.com>
2020-09-09 17:30:24 -0400

    Update Intern.js

--- Intern.js
+++ Intern.js
@@ -1,9 +1,24 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 class Intern {
+  const Employee = require("./Employee");
 
-    constructor(name,duty) {
+  class Intern extends Employee{
+    constructor(name,id,email,school){
+      super(name,id,email);
       this.name = name;
-      this.duty = duty; 
-    }
+      this.duty = id;
+      this.email = email; 
+      this.school = school;
+    };
+    getSchool(){
+      console.log(this.name);
+      console.log(this.email);
+      console.log(this.id);
+      return this.school;
+
+    };
+    getRole(){
+      return "Intern";
+    };
 }
 module.exports = Intern;
